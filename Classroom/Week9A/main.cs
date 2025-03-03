b674f6f5c0bfd0cef18e58fd268d4659011b9e67
macbeth-byui <macbethc@byui.edu>
2025-03-03 11:43:04 -0700

    Updates

--- Classroom/Week9A/main.cs
+++ Classroom/Week9A/main.cs
@@ -63,13 +63,13 @@ class Program
 
         Console.WriteLine("----");
 
-        a = 5;
-        b = 1176;
-        Console.WriteLine($"a = {a} b = {b}");
-        (gcd,i,j) = RSA.Euclid(a,b);
-        Console.WriteLine($"gcd = {gcd} i = {i} j = {j}"); 
-        d = RSA.ModInverse(i,b);
-        Console.WriteLine($"d (inverse) = {d}");
-        Console.WriteLine($"(d * a) mod b = {(d*a) % b}");
+        // BigInteger a2 = 53728375723;
+        // BigInteger b2 = 987273938592358421;
+        // Console.WriteLine($"a = {a2} b = {b2}");
+        // var (gcd2,i2,j2) = RSA.Euclid(a2,b2);
+        // Console.WriteLine($"gcd = {gcd2} i = {i2} j = {j2}"); 
+        // var d2 = RSA.ModInverse(i2,b2);
+        // Console.WriteLine($"d (inverse) = {d2}");
+        // Console.WriteLine($"(d * a) mod b = {(d2*a2) % b2}");
     }
 }
